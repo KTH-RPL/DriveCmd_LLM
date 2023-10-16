d3f2f88f2c24d627a0d184a7ddcb6e170ab8eaf0
Kin <kinzhangglimmer@gmail.com>
2023-10-16 12:10:13 +0200

    feat(ing): try data.

--- scripts/main_llc.py
+++ scripts/main_llc.py
@@ -6,15 +6,35 @@ from typing import Optional
 import fire
 
 from llama import Llama
+import pandas as pd
+DEFAULT_SYSTEM_PROMPT = """\
+Now I am providing you a command that a person can send to self-driving vehicle. \
+Could you say whether this command needs to use how many of the below sections? \
+It includes Perception, In-cabin monitoring, Localization, Vehicle control, Entertainmen, Personal data, Network access, Traffic laws. \
+For example, I provide "Drive to the nearest train station.", Then it should include yes for Perception, Localization, Vehicle control, Network access.\
+So you should output corresponding yes to these and no for others. Then you should output "1 0 1 1 0 0 1 0" to me. \
+Now Tell me if the command is:"""
+
+def read_all_command(path: str):
+    commands_df = pd.read_csv(path, encoding='ISO-8859-1')
+    preview = commands_df.head()
+    # Detailed analysis of each command
+    all_commands_only = []
+    task_name = preview.columns.to_list()[2:]
+    for index, row in commands_df.iterrows():
+        command = row["Command"].lower()
+        all_commands_only.append(command)
+    return all_commands_only, task_name
 
 
 def main(
-    ckpt_dir: str,
-    tokenizer_path: str,
+    ckpt_dir: str = "model/CodeLlama-7b",
+    tokenizer_path: str = "model/CodeLlama-7b/tokenizer.model",
+    csv_path: str = "assets/ucu.csv",
     temperature: float = 0.2,
     top_p: float = 0.95,
-    max_seq_len: int = 512,
-    max_batch_size: int = 8,
+    max_seq_len: int = 128,
+    max_batch_size: int = 4,
     max_gen_len: Optional[int] = None,
 ):
     generator = Llama.build(
@@ -23,31 +43,14 @@ def main(
         max_seq_len=max_seq_len,
         max_batch_size=max_batch_size,
     )
-
-    instructions = [
-        [
-            {
-                "role": "user",
-                "content": "In Bash, how do I list all text files in the current directory (excluding subdirectories) that have been modified in the last month?",
-            }
-        ],
-        [
-            {
-                "role": "user",
-                "content": "What is the difference between inorder and preorder traversal? Give an example in Python.",
-            }
-        ],
-        [
-            {
-                "role": "system",
-                "content": "Provide answers in JavaScript",
-            },
-            {
-                "role": "user",
-                "content": "Write a function that computes the set of sums of all contiguous sublists of a given list.",
-            }
-        ],
-    ]
+    
+    commands, tasks = read_all_command(csv_path)
+    instructions = []
+    for i, command in enumerate(commands):
+        instructions.append([{"role": "user", "content": DEFAULT_SYSTEM_PROMPT + command}])
+        if i>5: # debugging now
+            break
+        
     results = generator.chat_completion(
         instructions,  # type: ignore
         max_gen_len=max_gen_len,
