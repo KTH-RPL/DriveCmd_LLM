b53e54edba9ec1e7fc3c156cc3ef74e805c1f19b
lic <cil@kth.se>
2023-10-20 21:56:02 +0200

    fix:gpt ablation study

---
+++ assets/slurm/gpt_ablation_study_param.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+#SBATCH -A berzelius-2023-256
+#SBATCH -J gpt
+#SBATCH --gpus 0
+#SBATCH -t 3-00:00:00
+#SBATCH --mail-type=END,FAIL
+#SBATCH --mail-user=cil@kth.se
+#SBATCH --output /home/x_cili/x_cili_yy/llc/assets/logs/%J_gpt_param.out
+#SBATCH --error  /home/x_cili/x_cili_yy/llc/assets/logs/%J_gpt_param.err
+
+
+# Apptainer images can only be used outside /home. In this example the
+# image is located here
+cd /home/x_cili/x_cili_yy/llc
+module load Anaconda/2021.05-nsc1
+
+start="0"
+
+/home/x_cili/x_cili_lic/conda/envs/llc/bin/python scripts/main_gpt.py --debug_len=-1 --start_from "$start"  \
+ --slurm_job_id $SLURM_JOB_ID \
+ --provide_detailed_explain False --provide_few_shots False --step_by_step False 
+
+
+/home/x_cili/x_cili_lic/conda/envs/llc/bin/python scripts/main_gpt.py --debug_len=-1 --start_from "$start"  \
+ --slurm_job_id $SLURM_JOB_ID \
+ --provide_detailed_explain True --provide_few_shots False --step_by_step False 
+ 
+
+/home/x_cili/x_cili_lic/conda/envs/llc/bin/python scripts/main_gpt.py --debug_len=-1 --start_from "$start"  \
+ --slurm_job_id $SLURM_JOB_ID \
+ --provide_detailed_explain False --provide_few_shots False --step_by_step True 
+ 
+ 
+
